f3ef9048ea54dbb73e079b1896499b02b15625c3
jkoziolk <jkoziolk@wp.pl>
2016-12-25 19:45:29 +0100

    Project is created.

---
+++ CPOO disparity/CPOO disparity/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CPOO_disparity
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
