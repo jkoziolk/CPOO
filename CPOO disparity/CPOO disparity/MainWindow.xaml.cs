f21e6bf18700ddba3994d8599c715fb208f66386
jkoziolk <jkoziolk@wp.pl>
2016-12-26 13:05:00 +0100

    It is possible to open images.

--- CPOO disparity/CPOO disparity/MainWindow.xaml.cs
+++ CPOO disparity/CPOO disparity/MainWindow.xaml.cs
@@ -5,13 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.ExceptionServices;
+
 
 namespace CPOO_disparity
 {
@@ -25,10 +39,50 @@ namespace CPOO_disparity
             InitializeComponent();
         }
 
+        private Bitmap bitmapL;
+        private Bitmap bitmapR;
+        private Bitmap bitmapRes;
+
         private void Open_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            OpenDialog openDialog = new OpenDialog();
+            if (openDialog.ShowDialog() == true)
+            {
+                OpenBitmaps(openDialog);
+                BitmapsToUI();
+                Reset();
+            }
         }
+
+        private void OpenBitmaps(OpenDialog dlg)
+        {
+            bitmapL = OpenBmpFromDir(dlg.leftPath);
+            bitmapR = OpenBmpFromDir(dlg.rightPath);
+        }
+
+        private Bitmap OpenBmpFromDir(String dir)
+        {
+            if (dir.EndsWith(".png"))
+                return new Bitmap(dir);
+            else
+                throw new InvalidDataException("Only png images are valid.");
+        }
+
+        private void BitmapsToUI()
+        {
+            BitmapImage left = BitmapToBitmapImageConverter.Convert(bitmapL);
+            LeftImage.Source = left;
+
+            BitmapImage right = BitmapToBitmapImageConverter.Convert(bitmapR);
+            RightImage.Source = right;
+        }
+
+        private void Reset()
+        {
+            ResultImage.Source = null;
+            bitmapRes = null;
+        }
+
 
         private void Save_MouseDown(object sender, MouseButtonEventArgs e)
         {
---
+++ CPOO disparity/CPOO disparity/OpenDialog.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CPOO_disparity
+{
+    public partial class OpenDialog : Window
+    {
+        public OpenDialog()
+        {
+            InitializeComponent();
+        }
+
+        private String _leftPath = "";
+        public String leftPath
+        {
+            get { return _leftPath; }
+        }
+
+        private String _rightPath = "";
+        public String rightPath
+        {
+            get { return _rightPath; }
+        }
+
+        private string initialDirectory = "C:\\";
+
+        private void OpenLeft_Click(object sender, RoutedEventArgs e)
+        {
+            var imageDialog = OpenImageDialog();
+            bool? userClickedOK = imageDialog.ShowDialog();
+
+            if (userClickedOK == true)
+            {
+                _leftPath = imageDialog.FileName;
+                LeftPath.Text = imageDialog.FileName;
+                System.IO.FileInfo fInfo = new System.IO.FileInfo(_leftPath);
+                initialDirectory = fInfo.DirectoryName;
+            }
+        }
+
+        private void OpenRight_Click(object sender, RoutedEventArgs e)
+        {
+            var imageDialog = OpenImageDialog();
+            bool? userClickedOK = imageDialog.ShowDialog();
+
+            if (userClickedOK == true)
+            {
+                _rightPath = imageDialog.FileName;
+                RightPath.Text = imageDialog.FileName;
+                System.IO.FileInfo fInfo = new System.IO.FileInfo(_rightPath);
+                initialDirectory = fInfo.DirectoryName;
+            }
+        }
+
+
+        private OpenFileDialog OpenImageDialog()
+        {
+            var openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = initialDirectory;
+            openFileDialog1.Filter = "Image Files (*.png)|*.png;";
+            openFileDialog1.Multiselect = false;
+            return openFileDialog1;
+        }
+
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (_leftPath != "" && _rightPath != "")
+                this.DialogResult = true;
+            else
+                this.DialogResult = false;
+        }
+    }
+}
