bcd829c760953fecd9a933bfe6fe6284d5adbff0
Pawel <pawelwski24@gmail.com>
2017-01-04 15:41:07 +0100

    Pseudocoloring done with HSV. Works better.

--- CPOO disparity/CPOO disparity/Postprocessor.cs
+++ CPOO disparity/CPOO disparity/Postprocessor.cs
@@ -136,6 +136,104 @@ namespace CPOO_disparity
             return res;
         }
 
+        public static void HsvToRgb(double h, double S, double V, out int r, out int g, out int b)
+        {
+            double H = h;
+            while (H < 0) { H += 360; };
+            while (H >= 360) { H -= 360; };
+            double R, G, B;
+            if (V <= 0)
+            { R = G = B = 0; }
+            else if (S <= 0)
+            {
+                R = G = B = V;
+            }
+            else
+            {
+                double hf = H / 60.0;
+                int i = (int)Math.Floor(hf);
+                double f = hf - i;
+                double pv = V * (1 - S);
+                double qv = V * (1 - S * f);
+                double tv = V * (1 - S * (1 - f));
+                switch (i)
+                {
+
+                    // Red is the dominant color
+
+                    case 0:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+
+                    // Green is the dominant color
+
+                    case 1:
+                        R = qv;
+                        G = V;
+                        B = pv;
+                        break;
+                    case 2:
+                        R = pv;
+                        G = V;
+                        B = tv;
+                        break;
+
+                    // Blue is the dominant color
+
+                    case 3:
+                        R = pv;
+                        G = qv;
+                        B = V;
+                        break;
+                    case 4:
+                        R = tv;
+                        G = pv;
+                        B = V;
+                        break;
+
+                    // Red is the dominant color
+
+                    case 5:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
+
+                    case 6:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+                    case -1:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // The color is not defined, we should throw an error.
+
+                    default:
+                        //LFATAL("i Value error in Pixel conversion, Value is %d", i);
+                        R = G = B = V; // Just pretend its black/white
+                        break;
+                }
+            }
+            r = Clamp((int)(R * 255.0));
+            g = Clamp((int)(G * 255.0));
+            b = Clamp((int)(B * 255.0));
+        }
+
+        public static int Clamp(int i)
+        {
+            if (i < 0) return 0;
+            if (i > 255) return 255;
+            return i;
+        }
+
         public static Bitmap Pseudocolor(Bitmap img)
         {
             int w = img.Width;
@@ -151,6 +249,27 @@ namespace CPOO_disparity
                         //real pseudocolor function is needed
                         //outImg.SetPixel(x, y, new Rgb(inImg.GetPixel(x,y).R, 0,0));
 
+                        int black = inImg.GetPixel(x, y).G;
+                        double factor = black / 255.0;
+                        //scale invertion
+                        factor = factor * (-1) + 1;
+                        double hue = 360.0 * factor;
+                        //270 istead of 360 gives better results
+                        hue = 270.0 * factor;
+
+                        double sat = 0.8;
+                        double val = 1;
+
+                        int r = 0;
+                        int g = 0;
+                        int b = 0;
+
+                        HsvToRgb(hue, sat, val, out r, out g, out b);
+                        outImg.SetPixel(x, y, new Rgb(r, g, b));
+
+
+                        /*
+                        //ver 1
                         int black = inImg.GetPixel(x, y).G;
                       
 
@@ -188,6 +307,7 @@ namespace CPOO_disparity
                             green = 255;
                         }
                         outImg.SetPixel(x, y, new Rgb(red, green, blue));
+                        */
                     }
                 });
 
